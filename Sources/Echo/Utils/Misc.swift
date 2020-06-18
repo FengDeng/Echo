ac3b25c84234bd94437d7d0f5f70262b0e1cb9e1
RaiseChestnut <704292743@qq.com>
2020-06-18 10:50:41 +0800

    fix error: load from misaligned raw pointer

--- Sources/Echo/Utils/Misc.swift
+++ Sources/Echo/Utils/Misc.swift
@@ -50,7 +50,7 @@ protocol LayoutWrapper {
 
 extension LayoutWrapper {
   var layout: Layout {
-    ptr.load(as: Layout.self)
+    ptr.withMemoryRebound(to: Layout.self, capacity: 1) { $0.pointee }
   }
   
   var trailing: UnsafeRawPointer {
